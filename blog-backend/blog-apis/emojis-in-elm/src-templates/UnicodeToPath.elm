6535225ca531f1e8a5cd32302377a153561b1032
techmindful <hello@techmindful.blog>
2021-07-04 03:01:16 -0700

    Backend can create a modified version of source code based on user input.

---
+++ blog-backend/blog-apis/emojis-in-elm/src-templates/UnicodeToPath.elm
@@ -0,0 +1,23 @@
+module UnicodeToPath exposing (suite)
+
+import Expect
+import Test exposing (Test, test)
+
+
+correctUnicodeToPath : String -> String
+correctUnicodeToPath unicode =
+    "/static/noto-emoji/32/emoji_u" ++ unicode ++ ".png"
+
+
+suite : Test
+suite =
+    test
+        "Emoji unicode matches the image file path."
+        (\_ ->
+            userUnicodeToPath "1f600"
+                |> Expect.equal (correctUnicodeToPath "1f600")
+        )
+
+
+userUnicodeToPath : String -> String
+userUnicodeToPath unicode =
--- blog-backend/src/App.hs
+++ blog-backend/src/App.hs
@@ -33,18 +33,22 @@ import           Servant
   , serve
   , serveWithContext
   )
+import           Network.Wai as Wai
+import           Network.Wai.Handler.Warp as Warp
+import qualified Web.ClientSession as Session
 
+import           Conduit
 import           Control.Monad.IO.Class ( liftIO )
 import           Control.Monad.Reader ( ReaderT, runReaderT )
 import           Control.Concurrent.STM ( TVar, atomically, newTVar, readTVar, writeTVar )
 import           Data.Aeson ( FromJSON, ToJSON )
 import qualified Data.ByteArray as ByteArray
+import qualified Data.ByteString.Char8 as ByteStrC8
 import qualified Data.Map as Map
 import           Data.Map ( Map )
 import           GHC.Float ( rationalToFloat )
 import           GHC.Generics ( Generic )
-import           Network.Wai as Wai
-import           Network.Wai.Handler.Warp as Warp
+import           System.IO ( appendFile, readFile )
 
 
 type AppState = ()
@@ -59,11 +63,23 @@ type AppM = ReaderT AppState Handler
 
 server :: ServerT API AppM
 server =
+
   unicodeToPathHandler
+
   where
+
     unicodeToPathHandler :: String -> AppM String
     unicodeToPathHandler userCode = do
-      return "Unicode to path handler"
+
+      let path = "blog-apis/emojis-in-elm/src/UnicodeToPath.elm"
+
+      liftIO
+        $ runConduitRes
+        $ sourceFile path
+       .| mapC ( \str -> ByteStrC8.pack $ ByteStrC8.unpack str ++ userCode )
+       .| sinkFile "test.elm"
+       
+      return "Inserted user code"
 
 
 api :: Servant.Proxy API
@@ -77,6 +93,9 @@ mkApp appState =
 
 runApp :: IO ()
 runApp = do
+
+  sessionEncryptionKey <- Session.getDefaultKey
+  putStrLn $ show sessionEncryptionKey
 
   Warp.run 9000 $ mkApp ()
 
