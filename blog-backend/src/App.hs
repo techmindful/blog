9633a62e34753ee1fac0c481c750e0b4924f80e8
techmindful <hello@techmindful.blog>
2021-07-04 18:02:32 -0700

    User's Elm file can be created under src-users/ with random hash in file name.

--- blog-backend/src/App.hs
+++ blog-backend/src/App.hs
@@ -40,14 +40,22 @@ import qualified Web.ClientSession as Session
 import           Conduit
 import           Control.Monad.IO.Class ( liftIO )
 import           Control.Monad.Reader ( ReaderT, runReaderT )
+import           Control.Monad.Trans.Maybe ( MaybeT, runMaybeT )
 import           Control.Concurrent.STM ( TVar, atomically, newTVar, readTVar, writeTVar )
+import           Crypto.Random ( seedNew, seedToInteger )
+import           Crypto.Hash ( SHA256(..), hashWith )
 import           Data.Aeson ( FromJSON, ToJSON )
 import qualified Data.ByteArray as ByteArray
 import qualified Data.ByteString.Char8 as ByteStrC8
+import           Data.Function ( (&) )
 import qualified Data.Map as Map
 import           Data.Map ( Map )
+import           Data.Maybe ( fromMaybe )
 import           GHC.Float ( rationalToFloat )
 import           GHC.Generics ( Generic )
+import           Path ( (</>), Abs, Rel, Dir, File, Path )
+import qualified Path
+import           Path.IO ( doesFileExist )
 import           System.IO ( appendFile, readFile )
 
 
@@ -61,6 +69,35 @@ type API = "blog-apis" :> "emojis-in-elm" :> "unicode-to-path"
 type AppM = ReaderT AppState Handler
 
 
+getRandomHash :: IO String
+getRandomHash = do
+  seed <- seedNew
+  let seedStr = show $ seedToInteger seed
+      hash    = show $ hashWith SHA256 $ ByteStrC8.pack seedStr
+  return hash
+
+
+mkUniqueFilePath :: Path Rel Dir
+                 -> Maybe String
+                 -> Maybe String
+                 -> MaybeT IO ( Path Rel File )
+mkUniqueFilePath dirPath maybePrefix maybePostfix = do
+
+  randomHash <- liftIO getRandomHash
+
+  let fileNameStr = fromMaybe "" maybePrefix ++ randomHash ++ fromMaybe "" maybePostfix
+
+  fileName <- Path.parseRelFile fileNameStr
+
+  let filePath = dirPath </> fileName
+
+  isCollided <- liftIO $ doesFileExist filePath
+  if isCollided then
+    mkUniqueFilePath dirPath maybePrefix maybePostfix
+  else
+    return filePath
+
+
 server :: ServerT API AppM
 server =
 
@@ -71,15 +108,32 @@ server =
     unicodeToPathHandler :: String -> AppM String
     unicodeToPathHandler userCode = do
 
-      let path = "blog-apis/emojis-in-elm/src/UnicodeToPath.elm"
+      let elmDirPath = $(Path.mkRelDir "blog-apis/emojis-in-elm/")
 
-      liftIO
-        $ runConduitRes
-        $ sourceFile path
-       .| mapC ( \str -> ByteStrC8.pack $ ByteStrC8.unpack str ++ userCode )
-       .| sinkFile "test.elm"
-       
-      return "Inserted user code"
+          templatesDirPath = elmDirPath </> $(Path.mkRelDir "src-templates/")
+          templateFilePath = templatesDirPath </> $(Path.mkRelFile "UnicodeToPath.elm")
+
+          usersDirPath = elmDirPath </> $(Path.mkRelDir "src-users/")
+
+      maybeUserFilePath <-
+        liftIO $ runMaybeT $ mkUniqueFilePath
+          usersDirPath ( Just "UnicodeToPath_" ) ( Just ".elm" )
+
+      let mkUserFile :: MonadIO m => Path Rel File -> m ()
+          mkUserFile path =
+            liftIO
+              $ runConduitRes
+              $ sourceFile ( templateFilePath & Path.toFilePath )
+             .| mapC ( \str -> ByteStrC8.pack $ ByteStrC8.unpack str ++ userCode )
+             .| sinkFile ( path & Path.toFilePath )
+
+      case maybeUserFilePath of
+        Nothing ->
+          return "Error"
+
+        Just path -> do
+          mkUserFile path
+          return "Inserted user code"
 
 
 api :: Servant.Proxy API
