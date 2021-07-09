0e586ae2c33cb53d1f1e4f3f522d04b9ac71ce27
techmindful <hello@techmindful.blog>
2021-07-09 03:05:54 -0700

    Backend can parse elm-test result into ElmTestResult type, which covers compile failure, test failure, and pass.

--- blog-backend/src/App.hs
+++ blog-backend/src/App.hs
@@ -34,12 +34,13 @@ import           Network.Wai.Handler.Warp as Warp
 
 import           Control.Arrow ( left )
 import           Control.Error ( note )
-import           Control.Monad.Trans.Maybe ( MaybeT, runMaybeT )
 import           Crypto.Random ( seedNew, seedToInteger )
 import           Crypto.Hash ( SHA256(..), hashWith )
+import           Data.Aeson as Aeson ( FromJSON(..), decodeStrict, withObject, (.:) )
 import qualified Data.ByteString.Char8 as ByteStrC8
-import           RIO.ByteString ( readFile, writeFile )
-import           RIO.List ( initMaybe )
+--import           Optics ( (^.) )
+import           RIO.ByteString as ByteStr ( hGetContents, null, readFile, writeFile )
+import           RIO.List ( headMaybe, initMaybe )
 import qualified RIO.Text as Text
 import           Path
   ( (</>)
@@ -51,7 +52,7 @@ import           Path
   )
 import qualified Path
 import           Path.IO ( createDirIfMissing ) 
-import           System.Process ( createProcess, proc )
+import           System.Process ( CreateProcess(..), StdStream( CreatePipe ), createProcess, proc )
 import qualified System.Process as Proc
 
 
@@ -170,6 +171,50 @@ tryMkUserFile templateDirPath templateModuleName userDirPath codeMod = do
           pure userFilePath
 
 
+data ElmTestResult
+  = CompileFailure ByteString
+  | TestFailure ElmTestExpected ElmTestActual
+  | Pass
+  | JsonError
+  deriving ( Generic, Show )
+
+newtype ElmTestExpected = ElmTestExpected
+  { getElmTestExpected :: Text }
+  deriving ( Generic, Show )
+newtype ElmTestActual = ElmTestActual
+  { getElmTestActual   :: Text }
+  deriving ( Generic, Show )
+
+
+data ElmTestJson = ElmTestJson
+  { event  :: Text
+  , status :: Text
+  , failures :: [ ElmTestJson_Failure ]
+  } deriving ( Generic, Show )
+instance FromJSON ElmTestJson
+
+data ElmTestJson_Failure = ElmTestJson_Failure
+  { reason :: ElmTestJson_Failure_Reason
+  } deriving ( Generic, Show )
+instance FromJSON ElmTestJson_Failure
+
+data ElmTestJson_Failure_Reason = ElmTestJson_Failure_Reason
+  { data_ :: ElmTestJson_Failure_Reason_Data
+  } deriving ( Generic, Show )
+-- Hand-writing instance, because "data" field name collides with reserved keyword.
+instance FromJSON ElmTestJson_Failure_Reason where
+  parseJSON =
+    withObject "data" $ \v ->
+      ElmTestJson_Failure_Reason <$> v .: "data"
+
+data ElmTestJson_Failure_Reason_Data = ElmTestJson_Failure_Reason_Data
+  { expected :: Text
+  , actual   :: Text
+  } deriving ( Generic, Show )
+instance FromJSON ElmTestJson_Failure_Reason_Data
+
+--{"event":"testCompleted","status":"fail","labels":["UnicodeToPath_fb05414c9993917f91a9c9b25d012aa0f0293e51eb33567c19ca51eceb4a07f4","Emoji unicode matches the image file path."],"failures":[{"given":null,"message":"Expect.equal","reason":{"type":"Equality","data":{"expected":"\"/static/noto-emoji/32/emoji_u1f600.png\"","actual":"\"test\"","comparison":"Expect.equal"}}}],"duration":"1"}
+
 server :: ServerT API AppM
 server =
 
@@ -195,20 +240,75 @@ server =
             initStr <- note EmptyTemplate $ initMaybe $ ByteStrC8.unpack templateCode
             pure $ ByteStrC8.pack $ initStr ++ userCode
 
-      let testElm :: MonadIO m => Path Rel File -> MaybeT m ()
-          testElm path = liftIO $ do
+      let testElm :: Path Rel File -> IO ElmTestResult
+          testElm path = do
 
             -- Path after setting directory
             path' <- Path.stripProperPrefix elmDirPath path
 
-            _ <- liftIO $ createProcess
-              ( proc "elm-test" [ toFilePath path' ] )
-              { Proc.cwd = Just $ toFilePath elmDirPath }
+            ( _, Just hout, Just herr, _ ) <- liftIO $ createProcess
+              ( proc "elm-test" [ toFilePath path', "--report", "json" ] )
+              { std_out = CreatePipe
+              , std_err = CreatePipe
+              , Proc.cwd = Just $ toFilePath elmDirPath
+              }
 
-            return ()
+            -- If compiling succeeds, stdout gives test events in jsons.
+            out <- hGetContents hout
+            -- If compiling fails, stderr gives compiler errors.
+            err <- hGetContents herr
+
+            -- Compile fails
+            if not $ ByteStr.null err then
+              pure $ CompileFailure err
+            -- Compile succeeds.
+            else do
+              -- Filter out test events unrelated to the result.
+              let  decodedJsons :: [ ElmTestJson ]
+                   decodedJsons = mapMaybe Aeson.decodeStrict $ ByteStrC8.lines out
+
+              case decodedJsons of
+                -- Single result:
+                elmTestJson : [] ->
+
+                  -- Test fails.
+                  if ( elmTestJson & status ) == "fail" then do
+
+                    let maybeResult :: Maybe ( ElmTestExpected, ElmTestActual )
+                        maybeResult = do
+
+                          failure <- headMaybe $ elmTestJson & failures
+
+                          -- TODO: Use lenses.
+                          let data__ = failure & reason & data_
+
+                          pure
+                            ( ElmTestExpected $ data__ & expected
+                            , ElmTestActual   $ data__ & actual
+                            )
+
+                    case maybeResult of
+                      Nothing -> pure JsonError
+                      Just ( expected_, actual_ ) ->
+                        pure $ TestFailure expected_ actual_
+
+                  -- Test passes.
+                  else
+                    pure Pass
+                          
+                [] ->
+                  pure JsonError
+
+                _ : _ -> do
+                  putStrLn "Multiple test runs?"
+                  pure JsonError
+
 
       userFilePath <- liftIO $ tryMkUserFile templatesDirPath templateModuleName usersDirPath codeMod
-      _ <- runMaybeT $ testElm userFilePath
+
+      elmTestResult <- liftIO $ testElm userFilePath
+
+      liftIO $ putStrLn $ show elmTestResult
 
       return "Inserted user code"
 
